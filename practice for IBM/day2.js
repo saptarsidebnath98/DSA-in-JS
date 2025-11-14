410c14fa01d62a767030e1f90ce07a4750a56eb0
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2025-11-14 12:49:02 +0530

    CHATGPT-Practice-lengthOfLongestSubString

--- practice for IBM/day2.js
+++ practice for IBM/day2.js
@@ -54,3 +54,23 @@ const firstNonRepeatingChar = (s) => {
 };
 // firstNonRepeatingChar("aabb")
 //-------------------------------------------------------------------------------------------------------------
+
+const lengthOfLongestSubString = (s) => {
+  let left = 0;
+  let maxLength = 0;
+  const set = new Set();
+  
+  for(let right = 0; right < s.length; right++){
+    while(set.has(s[right])){
+      set.delete(s[left]);
+      left++
+    }
+    set.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1)
+  }
+  
+  return maxLength
+};
+
+// lengthOfLongestSubString("aabb")
+//-------------------------------------------------------------------------------------------------------------
